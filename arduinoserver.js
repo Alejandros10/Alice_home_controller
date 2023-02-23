d5060625a08ec93d3e771a42d743e30383951feb
Alejandro Sanchez <alejandro.sanchez@doctus.com.co>
2023-02-23 00:08:53 -0500

    ALICE - Update server arduino

--- arduinoserver.js
+++ arduinoserver.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const Readline = require('@serialport/parser-readline');
 const SerialPort = require('serialport');
 
 // Configuración del puerto serie
-const port = new SerialPort('/dev/ttyACM0', { baudRate: /dev/ttyACM0 });
-
+const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
 // Creación del servidor Express
 const app = express();
 
@@ -18,6 +19,10 @@ app.get('/led/off', (req, res) => {
 port.write('0');
 res.send('LED apagado');
 });
+
+parser.on('data', data =>{
+    console.log('got word from arduino:', data);
+  });
 
 // Inicio del servidor
 app.listen(3000, () => {
